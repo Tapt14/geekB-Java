15cd5d039b8c013a0bfd208b2bccfc3d9115a984
Bolshov Maxim <bolshovm@yandex.ru>
2020-09-14 19:11:32 +0300

    add Lesson7

---
+++ Java3/Lesson7/Main.java
@@ -0,0 +1,11 @@
+package Java3.Lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        TestClass classForTesting = new TestClass();
+        TestsHandler.start(classForTesting.getClass());
+    }
+}
+
+
+
